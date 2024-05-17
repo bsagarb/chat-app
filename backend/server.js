8565369eeb7bdf9ba90185097f04b02e3219359a
bsagarb <bongusagar123@gmailcom>
2024-05-17 18:05:17 +0530

    4th completed

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js"
+import { app , server } from "./socket/socket.js"; 
 
-const app = express()
+
 
 dotenv.config()
 const PORT = process.env.PORT || 5000;
@@ -15,8 +16,8 @@ app.use(express.json()); // to parse incoming request from (req.body)
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes)
-app.use('/api/message',messageRoutes)
-app.use('/api/user',userRoutes)
+app.use('/api/messages',messageRoutes)
+app.use('/api/users',userRoutes)
 
 // app.get('/',(req,res)=>{
 //     res.send("hellow world")
@@ -25,7 +26,7 @@ app.use('/api/user',userRoutes)
 
 
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
      connectToMongoDB()
      console.log(`port running at ${PORT}`)
     })
