e17867013ed4b640f98890c89112df4982f802d0
bsagarb <bongusagar123@gmailcom>
2024-05-17 21:01:21 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -10,6 +11,7 @@ import { app , server } from "./socket/socket.js";
 
 
 dotenv.config()
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse incoming request from (req.body)
@@ -19,10 +21,11 @@ app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
 
-// app.get('/',(req,res)=>{
-//     res.send("hellow world")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
